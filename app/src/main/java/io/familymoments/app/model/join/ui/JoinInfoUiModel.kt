8bc1b776c44ed80078ef303e4897305dc3b79232
Hyegyeong <gju06061@naver.com>
2024-01-21 19:28:49 +0900

    [FMA-10] feat: add default value

--- app/src/main/java/io/familymoments/app/model/join/ui/JoinInfoUiModel.kt
+++ app/src/main/java/io/familymoments/app/model/join/ui/JoinInfoUiModel.kt
@@ -3,11 +3,11 @@ package io.familymoments.app.model.join.ui
 import android.graphics.Bitmap
 
 data class JoinInfoUiModel(
-        val id: String,
-        val password: String,
-        val name: String,
-        val email: String,
-        val birthDay: String,
-        val nickname: String,
-        val bitmap: Bitmap,
+        val id: String = "",
+        val password: String = "",
+        val name: String = "",
+        val email: String = "",
+        val birthDay: String = "",
+        val nickname: String = "",
+        val bitmap: Bitmap ,
 )
