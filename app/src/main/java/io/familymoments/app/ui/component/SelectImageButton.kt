2d50f48c74ea17cd0aa8e6cc052c36c19620ac32
Hyegyeong <gju06061@naver.com>
2024-02-05 16:08:56 +0900

    [FMA-12] feat: implement SelectImageButton

---
+++ app/src/main/java/io/familymoments/app/ui/component/SelectImageButton.kt
@@ -0,0 +1,59 @@
+package io.familymoments.app.ui.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import io.familymoments.app.R
+
+@Composable
+fun SelectImageButton(
+    modifier: Modifier = Modifier,
+    isDropDownMenuExist: Boolean,
+    menuItems: List<FMDropDownMenuItem> = listOf()
+) {
+    var isMenuExpanded: Boolean by remember { mutableStateOf(false) }
+    Button(
+        modifier = modifier,
+        onClick = { isMenuExpanded = !isMenuExpanded },
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = Color(0xFFF3F4F7),
+        ),
+        elevation = ButtonDefaults.elevation(defaultElevation = 0.dp, pressedElevation = 0.dp),
+    ) {
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(115.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Image(
+                modifier = Modifier.padding(bottom = 2.dp),
+                painter = painterResource(id = R.drawable.ic_select_pic),
+                contentDescription = null,
+            )
+            Text(text = stringResource(R.string.join_select_profile_image_btn), color = Color(0xFFBFBFBF))
+            if (isDropDownMenuExist) {
+                FMDropDownMenu(menuItems = menuItems, isMenuExpanded) { isMenuExpanded = false }
+            }
+        }
+    }
+}
