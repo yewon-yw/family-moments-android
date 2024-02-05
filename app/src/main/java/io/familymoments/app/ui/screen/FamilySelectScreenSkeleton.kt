b005096180d8998bcf3854b6112f91f540f4e2c4
Hyegyeong <gju06061@naver.com>
2024-02-05 16:07:56 +0900

    [FMA-12] feat: implement FamilySelectScreenSkeleton.kt

---
+++ app/src/main/java/io/familymoments/app/ui/screen/FamilySelectScreenSkeleton.kt
@@ -0,0 +1,65 @@
+package io.familymoments.app.ui.screen
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import io.familymoments.app.ui.theme.AppColors
+import io.familymoments.app.ui.theme.AppTypography
+
+@Composable
+fun CreateFamilyLayoutSkeleton(
+    headerBottomPadding: Dp,
+    header: String = "",
+    button: String = "",
+    onClick: () -> Unit = {},
+    content: @Composable () -> Unit
+) {
+
+    Column(modifier = Modifier.padding(horizontal = 16.dp)) {
+        Header(headerBottomPadding, header)
+        Box(modifier = Modifier.weight(1f)) {
+            content()
+
+        }
+        NextButton(button, onClick)
+    }
+
+}
+
+@Composable
+private fun Header(headerBottomPadding: Dp, header: String) {
+    Text(
+        modifier = Modifier.padding(top = 55.dp, bottom = headerBottomPadding),
+        text = header,
+        style = AppTypography.SH2_18,
+        color = AppColors.deepPurple1
+    )
+}
+
+@Composable
+private fun NextButton(button: String, onClick: () -> Unit) {
+    Button(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 100.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = AppColors.deepPurple1,
+            disabledContainerColor = AppColors.grey3
+        ),
+        shape = RoundedCornerShape(60.dp),
+        contentPadding = PaddingValues(vertical = 18.dp),
+        onClick = onClick
+    ) {
+        Text(text = button, style = AppTypography.BTN4_18, color = AppColors.grey6)
+    }
+}
