f0557abb82032386d2bf156f4a32c3ca0bbeef4b
hong <sdhong0609@gmail.com>
2024-03-03 23:00:04 +0900

    feat: Implement Add Post Screen
    fix: fix screen
    
    feat: Implement add pics on AddPost

--- app/src/main/java/io/familymoments/app/feature/bottomnav/graph/BottomNavGraph.kt
+++ app/src/main/java/io/familymoments/app/feature/bottomnav/graph/BottomNavGraph.kt
@@ -9,6 +9,7 @@ import io.familymoments.app.core.graph.CommonRoute
 import io.familymoments.app.core.graph.Route
 import io.familymoments.app.core.util.scaffoldState
 import io.familymoments.app.feature.album.screen.AlbumScreen
+import io.familymoments.app.feature.addpost.AddPostScreen
 import io.familymoments.app.feature.bottomnav.model.BottomNavItem
 import io.familymoments.app.feature.calendar.screen.CalendarScreen
 import io.familymoments.app.feature.home.screen.HomeScreen
@@ -41,7 +42,9 @@ fun NavGraphBuilder.bottomNavGraph(navController: NavController) {
     }
 
     composable(route = BottomNavItem.AddPost.route) {
-        // AddPostScreen()
+        AddPostScreen(
+            modifier = Modifier.scaffoldState(hasShadow = true, hasBackButton = true)
+        )
     }
 
     composable(route = BottomNavItem.Calendar.route) {
