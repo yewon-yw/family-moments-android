885535060db39dd25b0e7c5df477308cae15baaf
yewon-yw <kimyewon0401@gmail.com>
2024-03-09 01:49:28 +0900

    feat: apply global layout listener

--- app/src/main/java/io/familymoments/app/feature/modifypassword/screen/ModifyPasswordScreen.kt
+++ app/src/main/java/io/familymoments/app/feature/modifypassword/screen/ModifyPasswordScreen.kt
@@ -1,6 +1,9 @@
 package io.familymoments.app.feature.modifypassword.screen
 
+import android.content.res.Resources
+import android.view.ViewTreeObserver
 import androidx.annotation.StringRes
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,17 +11,24 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
@@ -31,24 +41,62 @@ import io.familymoments.app.core.component.FMTextField
 import io.familymoments.app.core.theme.AppColors
 import io.familymoments.app.core.theme.AppTypography
 import io.familymoments.app.feature.modifypassword.viewmodel.ModifyPasswordViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun ModifyPasswordScreen(
     modifier: Modifier = Modifier,
     viewModel: ModifyPasswordViewModel
 ) {
+    val scope = rememberCoroutineScope()
+    val scrollState = rememberScrollState()
+    var keyboardHeight by remember { mutableFloatStateOf(0f) }
+    KeyboardHeightListener { height ->
+        val adjustedHeight = if (height < 0) 0 else height
+        keyboardHeight = pixelsToDp(adjustedHeight)
+    }
+
     Column(
-        modifier = modifier.padding(horizontal = 16.dp)
+        modifier = modifier
+            .padding(horizontal = 16.dp)
+            .verticalScroll(scrollState)
     ) {
         ModifyPasswordTitle()
         Spacer(modifier = Modifier.height(40.dp))
         ModifyPasswordInfo()
         Spacer(modifier = Modifier.height(26.dp))
         CurrentPasswordField(viewModel = viewModel)
-        NewPasswordField(viewModel = viewModel)
+        NewPasswordField(viewModel = viewModel, scope = scope, scrollState = scrollState)
         ModifyPasswordButton(viewModel = viewModel)
-        Spacer(modifier = Modifier.height(20.dp))
+        Spacer(modifier = Modifier.height(keyboardHeight.dp + 20.dp))
     }
+}
+
+@Composable
+fun KeyboardHeightListener(onKeyboardHeightChanged: (Int) -> Unit) {
+    val context = LocalContext.current
+    val rootView = LocalView.current
+
+    DisposableEffect(context, rootView) {
+        val listener = ViewTreeObserver.OnGlobalLayoutListener {
+            val rect = android.graphics.Rect()
+            rootView.getWindowVisibleDisplayFrame(rect)
+            val screenHeight = rootView.height
+            val keyboardHeight = screenHeight - rect.bottom
+            onKeyboardHeightChanged(keyboardHeight)
+        }
+        rootView.viewTreeObserver.addOnGlobalLayoutListener(listener)
+        onDispose {
+            rootView.viewTreeObserver.removeOnGlobalLayoutListener(listener)
+        }
+    }
+}
+
+fun pixelsToDp(pixels: Int): Float {
+    val density = Resources.getSystem().displayMetrics.density
+    return pixels / density
 }
 
 @Composable
@@ -102,11 +150,22 @@ private fun CurrentPasswordField(viewModel: ModifyPasswordViewModel) {
 }
 
 @Composable
-private fun NewPasswordField(viewModel: ModifyPasswordViewModel) {
+private fun NewPasswordField(
+    viewModel: ModifyPasswordViewModel,
+    scope: CoroutineScope,
+    scrollState: ScrollState
+) {
     var newPassword by remember { mutableStateOf(TextFieldValue()) }
     var newPasswordCheck by remember { mutableStateOf(TextFieldValue()) }
     val newPasswordWarning = viewModel.newPasswordWarning.collectAsStateWithLifecycle().value
-
+    val onFocusChange: (Boolean) -> Unit = { isFocused ->
+        if (isFocused) {
+            scope.launch {
+                delay(300)
+                scrollState.animateScrollTo(scrollState.maxValue)
+            }
+        }
+    }
     ModifyPasswordTextField(
         onValueChange = {
             newPassword = it
@@ -114,7 +173,8 @@ private fun NewPasswordField(viewModel: ModifyPasswordViewModel) {
         },
         value = newPassword,
         hintResId = R.string.modify_password_new_password,
-        borderColor = if (newPasswordWarning == null) AppColors.grey2 else AppColors.red2
+        borderColor = if (newPasswordWarning == null) AppColors.grey2 else AppColors.red2,
+        onFocusChange = onFocusChange
     )
     Spacer(modifier = Modifier.padding(top = 18.dp))
     ModifyPasswordTextField(
@@ -124,7 +184,8 @@ private fun NewPasswordField(viewModel: ModifyPasswordViewModel) {
         },
         value = newPasswordCheck,
         hintResId = R.string.modify_password_new_password_check,
-        borderColor = if (newPasswordWarning == null) AppColors.grey2 else AppColors.red2
+        borderColor = if (newPasswordWarning == null) AppColors.grey2 else AppColors.red2,
+        onFocusChange = onFocusChange
     )
     ModifyPasswordWarning(
         warningResId = newPasswordWarning?.stringResId,
