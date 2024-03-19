9e67e78f15060868d38ea02798e55b88d863ec02
Hyegyeong <gju06061@naver.com>
2024-03-19 22:34:57 +0900

    feat: implement PostDetailScreen dropDownMenu composable

--- app/src/main/java/io/familymoments/app/feature/postdetail/screen/PostDetailScreen.kt
+++ app/src/main/java/io/familymoments/app/feature/postdetail/screen/PostDetailScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
@@ -27,6 +28,8 @@ import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -40,6 +43,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
@@ -55,13 +59,19 @@ import io.familymoments.app.feature.home.component.postItemContentShadow
 fun PostDetailScreen(
     modifier: Modifier
 ) {
-    Column(modifier = modifier) {
-        WriterInfo()
-        Spacer(modifier = Modifier.height(15.dp))
-        Divider(Modifier.height(1.dp), color = AppColors.deepPurple3)
-        Spacer(modifier = Modifier.height(19.dp))
-        PostBox()
+    LazyColumn {
+        item {
+            Column(modifier = modifier.padding(top = 26.dp, bottom = 63.dp)) {
+                WriterInfo()
+                Spacer(modifier = Modifier.height(15.dp))
+                Divider(Modifier.height(1.dp), color = AppColors.deepPurple3)
+                Spacer(modifier = Modifier.height(19.dp))
+                PostBox()
+            }
+        }
+
     }
+
 }
 
 @Composable
@@ -107,57 +117,71 @@ fun PostBox() {
     }
 }
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun Post() {
     Column {
-        Box(
-            modifier = Modifier
-                .height(168.dp)
+        PostPhotos()
+        PostContent()
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun PostPhotos() {
+    Box(
+        modifier = Modifier
+            .height(168.dp)
+    ) {
+        val pagerState = rememberPagerState(pageCount = { 3 })
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier.fillMaxSize()
         ) {
-            val pagerState = rememberPagerState(pageCount = { 3 })
-            HorizontalPager(
-                state = pagerState,
-                modifier = Modifier.fillMaxSize()
-            ) {
-                Image(
+            Image(
+                modifier = Modifier
+                    .height(180.dp)
+                    .align(Alignment.Center),
+                painter = painterResource(id = R.drawable.img_sample_trees),
+                contentScale = ContentScale.Crop,
+                contentDescription = null
+            )
+        }
+        Row(
+            modifier = Modifier
+                .wrapContentHeight()
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 6.dp),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            repeat(pagerState.pageCount) { iteration ->
+                val color =
+                    if (pagerState.currentPage == iteration) AppColors.purple2 else AppColors.grey6
+                Box(
                     modifier = Modifier
-                        .height(180.dp)
-                        .align(Alignment.Center),
-                    painter = painterResource(id = R.drawable.img_sample_trees),
-                    contentScale = ContentScale.Crop,
-                    contentDescription = null
+                        .padding(horizontal = (3.5).dp)
+                        .clip(CircleShape)
+                        .background(color)
+                        .size(7.dp)
                 )
             }
-            Row(
-                modifier = Modifier
-                    .wrapContentHeight()
-                    .fillMaxWidth()
-                    .align(Alignment.BottomCenter)
-                    .padding(bottom = 6.dp),
-                horizontalArrangement = Arrangement.Center
-            ) {
-                repeat(pagerState.pageCount) { iteration ->
-                    val color =
-                        if (pagerState.currentPage == iteration) AppColors.purple2 else AppColors.grey6
-                    Box(
-                        modifier = Modifier
-                            .padding(horizontal = (3.5).dp)
-                            .clip(CircleShape)
-                            .background(color)
-                            .size(7.dp)
-                    )
-                }
-            }
         }
+    }
+}
 
+@Composable
+fun PostContent() {
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+    Box {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(start = 18.dp, end = 6.dp),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            androidx.compose.material3.Text(
+            Text(
                 modifier = Modifier
                     .heightIn(183.dp)
                     .padding(top = 11.dp, end = 10.dp)
@@ -170,19 +194,38 @@ fun Post() {
                 modifier = Modifier.padding(top = 5.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Icon(
-                    imageVector = ImageVector.vectorResource(R.drawable.ic_three_dots_row),
-                    tint = AppColors.deepPurple1,
-                    contentDescription = null,
-                    modifier = Modifier.noRippleClickable {
+                Box {
+                    Icon(
+                        imageVector = ImageVector.vectorResource(R.drawable.ic_three_dots_row),
+                        tint = AppColors.deepPurple1,
+                        contentDescription = null,
+                        modifier = Modifier.noRippleClickable {
+                            expanded = true
+                        }
+                    )
+                    PostDetailDropdownMenu(
+                        items = listOf(
+                            Pair(stringResource(id = R.string.post_detail_screen_drop_down_menu_modify), {}),
+                            Pair(stringResource(id = R.string.post_detail_screen_drop_down_menu_report), {}),
+                            Pair(stringResource(id = R.string.post_detail_screen_drop_down_menu_delete), {}),
+                        ),
+                        expanded = expanded
+                    ) { expanded = it }
+                }
 
-                    }
-                )
                 Spacer(modifier = Modifier.height(6.dp))
+
+                var commentLike by remember {
+                    mutableStateOf(false)
+                }
+
                 Icon(
-                    imageVector = ImageVector.vectorResource(R.drawable.ic_heart_empty),
+                    imageVector =
+                    if (!commentLike) ImageVector.vectorResource(R.drawable.ic_heart_empty)
+                    else ImageVector.vectorResource(R.drawable.ic_heart_filled),
                     contentDescription = null,
-                    tint = Color.Unspecified
+                    tint = Color.Unspecified,
+                    modifier = Modifier.noRippleClickable { commentLike = !commentLike }
                 )
                 Spacer(modifier = Modifier.height(3.dp))
                 androidx.compose.material3.Text(text = "3", style = AppTypography.LB2_11, color = AppColors.black2)
@@ -208,7 +251,7 @@ fun Comments() {
                 modifier = Modifier.weight(1f)
             )
             Text(
-                text = "하트 보기",
+                text = stringResource(R.string.post_detail_screen_show_heart),
                 style = AppTypography.B2_14,
                 color = AppColors.grey2
             )
@@ -216,7 +259,7 @@ fun Comments() {
         Spacer(modifier = Modifier.height(10.dp))
         CommentTextField()
         Spacer(modifier = Modifier.height(18.dp))
-        CommentItem()
+        CommentItems()
     }
 }
 
@@ -238,7 +281,9 @@ fun CommentTextField() {
         Row(modifier = Modifier.fillMaxHeight()) {
             BasicTextField(
                 value = comments,
-                onValueChange = { comments = it },
+                onValueChange = {
+                    if (it.text.length <= 50) comments = it
+                },
                 textStyle = AppTypography.LB2_11.copy(AppColors.black1),
                 modifier = Modifier
                     .weight(1f)
@@ -246,7 +291,7 @@ fun CommentTextField() {
             ) { innerTextField ->
                 if (comments.text.isEmpty()) {
                     Text(
-                        text = "댓글 작성 (50자 이내)",
+                        text = stringResource(R.string.post_detail_screen_comment_text_field_hint),
                         color = AppColors.grey3,
                         style = AppTypography.LB2_11
                     )
@@ -265,7 +310,7 @@ fun CommentTextField() {
                 contentPadding = PaddingValues(0.dp),
             ) {
                 Text(
-                    text = "작성",
+                    text = stringResource(R.string.post_detail_screen_comment_post_button),
                     style = AppTypography.BTN6_13,
                     color = AppColors.grey6
                 )
@@ -275,8 +320,22 @@ fun CommentTextField() {
 }
 
 @Composable
+fun CommentItems() {
+    Column(verticalArrangement = Arrangement.spacedBy(12.dp)) {
+        CommentItem()
+        CommentItem()
+        CommentItem()
+        CommentItem()
+    }
+
+}
+
+@Composable
 fun CommentItem() {
-    Box(
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+    Row(
         modifier = Modifier
             .fillMaxWidth()
             .padding(start = 10.dp, end = 12.dp)
@@ -285,35 +344,9 @@ fun CommentItem() {
                 shape = RoundedCornerShape(size = 8.dp)
             )
     ) {
-        Icon(
-            imageVector = ImageVector.vectorResource(R.drawable.ic_three_dots_row),
-            contentDescription = null,
-            modifier = Modifier
-                .padding(top = 3.dp, end = 6.dp)
-                .align(Alignment.TopEnd)
-                .noRippleClickable {
-                },
-            tint = Color.Unspecified,
-        )
         Column(
-            modifier = Modifier
-                .padding(end = 7.dp, bottom = 8.dp)
-                .align(Alignment.BottomEnd)
+            modifier = Modifier.weight(1f)
         ) {
-            Icon(
-                imageVector = ImageVector.vectorResource(R.drawable.ic_heart_empty),
-                contentDescription = null,
-                tint = Color.Unspecified,
-                modifier = Modifier.align(Alignment.End)
-            )
-            Text(
-                text = "7분 전",
-                style = AppTypography.LB2_11,
-                color = AppColors.grey3
-            )
-        }
-
-        Column {
             Row(modifier = Modifier.padding(start = 11.dp, top = 10.dp)) {
                 Image(
                     painter = painterResource(id = R.drawable.default_profile),
@@ -338,6 +371,90 @@ fun CommentItem() {
             )
         }
 
+        Column(
+            modifier = Modifier
+                .padding(end = 6.dp, bottom = 8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Box {
+                Icon(
+                    imageVector = ImageVector.vectorResource(R.drawable.ic_three_dots_row),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .padding(top = 3.dp)
+                        .noRippleClickable {
+                            expanded = true
+                        },
+                    tint = Color.Unspecified,
+                )
+                PostDetailDropdownMenu(
+                    items = listOf(
+                        Pair(stringResource(id = R.string.post_detail_screen_drop_down_menu_report), {}),
+                        Pair(stringResource(id = R.string.post_detail_screen_drop_down_menu_delete), {}),
+                    ),
+                    expanded = expanded
+                ) { expanded = it }
+            }
+
+            var commentLike by remember {
+                mutableStateOf(false)
+            }
+
+            Icon(
+                imageVector =
+                if (!commentLike) ImageVector.vectorResource(R.drawable.ic_heart_empty)
+                else ImageVector.vectorResource(R.drawable.ic_heart_filled),
+                contentDescription = null,
+                tint = Color.Unspecified,
+                modifier = Modifier
+                    .padding(top = 5.dp)
+                    .align(Alignment.End)
+                    .noRippleClickable { commentLike = !commentLike }
+            )
+            Text(
+                text = "7분 전",
+                style = AppTypography.LB2_11,
+                color = AppColors.grey3,
+                modifier = Modifier.padding(top = 4.dp)
+            )
+        }
+
+    }
+}
+
+@Composable
+fun PostDetailDropdownMenu(
+    items: List<Pair<String, () -> Unit>>,
+    modifier: Modifier = Modifier,
+    expanded: Boolean,
+    onExpandedChange: (Boolean) -> Unit
+) {
+    DropdownMenu(
+        expanded = expanded,
+        onDismissRequest = { onExpandedChange(false) },
+        modifier = modifier.then(
+            Modifier
+                .width(112.dp)
+                .background(color = AppColors.deepPurple3, shape = RoundedCornerShape(5.dp))
+        )
+    )
+    {
+        items.forEach {
+            DropdownMenuItem(
+                text = {
+                    Text(
+                        text = it.first,
+                        style = AppTypography.LB2_11,
+                        color = AppColors.grey6
+                    )
+                },
+                onClick = {
+                    onExpandedChange(false)
+                    it.second()
+                },
+                modifier = Modifier.height(35.dp)
+            )
+        }
     }
 }
 
@@ -351,6 +468,8 @@ fun PostDetailPreview() {
 
 @Preview(showBackground = true)
 @Composable
-fun WriterInfoPreview() {
-    WriterInfo()
+fun PostDetailDropdownMenuPreview() {
+    FamilyMomentsTheme {
+        PostDetailDropdownMenu(items = listOf(), modifier = Modifier, true) {}
+    }
 }
