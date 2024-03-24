ed15b64304fbae1f4af4f04a2d71c698b69cda50
Hyegyeong <gju06061@naver.com>
2024-03-25 03:19:06 +0900

    feat: apply code review

--- app/src/main/java/io/familymoments/app/core/component/GalleryOrDefaultImageSelectButton.kt
+++ app/src/main/java/io/familymoments/app/core/component/GalleryOrDefaultImageSelectButton.kt
@@ -25,8 +25,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import io.familymoments.app.R
 import io.familymoments.app.core.theme.AppColors
@@ -94,4 +96,11 @@ fun GalleryOrDefaultImageSelectButton(
             }
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GalleryOrDefaultImageSelectButtonPreview(){
+    val context = LocalContext.current
+    GalleryOrDefaultImageSelectButton(context) {}
 }
--- app/src/main/java/io/familymoments/app/core/component/GalleryOrDefaultImageSelectDropDownMenu.kt
+++ app/src/main/java/io/familymoments/app/core/component/GalleryOrDefaultImageSelectDropDownMenu.kt
@@ -9,28 +9,53 @@ import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import io.familymoments.app.R
 
 @Composable
 fun GalleryOrDefaultImageSelectDropDownMenu(
     launcher: ManagedActivityResultLauncher<PickVisualMediaRequest, Uri?>,
     getDefaultImageBitmap: () -> Unit,
-    isMenuExpanded: Boolean, onDismissRequest: () -> Unit
+    isMenuExpanded: Boolean,
+    onDismissRequest: () -> Unit
 ) {
-    DropdownMenu(expanded = isMenuExpanded, onDismissRequest = onDismissRequest) {
-        DropdownMenuItem(onClick = {
+    GalleryOrDefaultImageSelectDropDownMenu(
+        isMenuExpanded,
+        {
             launcher.launch(
                 PickVisualMediaRequest(mediaType = ActivityResultContracts.PickVisualMedia.ImageOnly)
             )
+        },
+        { getDefaultImageBitmap() },
+        onDismissRequest
+    )
+}
+
+@Composable
+fun GalleryOrDefaultImageSelectDropDownMenu(
+    isMenuExpanded: Boolean,
+    onGallerySelect: () -> Unit,
+    onDefaultImgSelect: () -> Unit,
+    onDismissRequest: () -> Unit
+) {
+    DropdownMenu(expanded = isMenuExpanded, onDismissRequest = onDismissRequest) {
+        DropdownMenuItem(onClick = {
+            onGallerySelect()
             onDismissRequest()
         }) {
             Text(text = stringResource(id = R.string.drop_down_menu_item_gallery_select))
         }
         DropdownMenuItem(onClick = {
-            getDefaultImageBitmap()
+            onDefaultImgSelect()
             onDismissRequest()
         }) {
             Text(text = stringResource(id = R.string.drop_down_menu_item_default_image))
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GalleryOrDefaultImageSelectDropDownMenuPreview() {
+    GalleryOrDefaultImageSelectDropDownMenu(true, {}, {}, {})
 }
