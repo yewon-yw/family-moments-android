40b07145c8b09884e0cc5fb320351468d61901dc
hong <sdhong0609@gmail.com>
2024-03-27 01:20:14 +0900

    refactor: refactor LoginScreen

--- app/src/main/java/io/familymoments/app/feature/login/screen/LoginScreen.kt
+++ app/src/main/java/io/familymoments/app/feature/login/screen/LoginScreen.kt
@@ -36,6 +36,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -78,10 +79,12 @@ fun LoginScreen(viewModel: LoginViewModel) {
         context.startActivity(Intent(context, SignUpActivity::class.java))
     }
 
-    if (loginUiState.value.isSuccess == true) {
-        val intent = Intent(context, MainActivity::class.java)
-        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-        context.startActivity(intent)
+    LaunchedEffect(loginUiState.value.isSuccess) {
+        if (loginUiState.value.isSuccess == true) {
+            val intent = Intent(context, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            context.startActivity(intent)
+        }
     }
 
     AppBarScreen(title = {
