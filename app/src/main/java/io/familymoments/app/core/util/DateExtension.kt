e0f75e06e771d24b455487221d057dfb56769192
yewon-yw <kimyewon0401@gmail.com>
2024-09-17 14:04:06 +0900

    fix: 게시글 날짜를 사용자 로컬 설정에 맞게 포맷팅 되도록 변경
    - Local.KOREA 에서 Local.getDefault()로 변경

--- app/src/main/java/io/familymoments/app/core/util/DateExtension.kt
+++ app/src/main/java/io/familymoments/app/core/util/DateExtension.kt
@@ -2,17 +2,17 @@ package io.familymoments.app.core.util
 
 import io.familymoments.app.core.network.dto.response.Post
 import io.familymoments.app.core.util.DateFormatter.dateTimeToLocalDate
-import java.text.SimpleDateFormat
-import java.util.Date
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import java.util.Locale
 
 // 게시글 날짜 출력 형식 지정
 fun String.formattedPostDate(): String {
-    val inputFormat = SimpleDateFormat("yyyy-MM-dd", Locale.KOREA)
-    val date = inputFormat.parse(this)
+    val inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.getDefault())
+    val date = LocalDate.parse(this, inputFormatter)
 
-    val outputFormat = SimpleDateFormat("yyyy.MM.dd(EEE)", Locale.KOREA)
-    return outputFormat.format(date ?: Date())
+    val outputFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd(EEE)", Locale.getDefault())
+    return date.format(outputFormatter)
 }
 
 // 게시글 날짜를 사용자의 로컬 시간대로 변환
