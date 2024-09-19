6377265eada3e4be6808b19a5b338b6c8d2a05b1
boring-km <kms0644804@naver.com>
2024-09-19 22:18:55 +0900

    version: 0.0.7 업데이트 - 출시 시도

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "io.familymoments.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 6
-        versionName = "0.0.6"
+        versionCode = 7
+        versionName = "0.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
